b900e01bc6db899909771bf1b219e80e453d6835
Deep Patel <dpagentx@gmail.com>
2022-07-09 13:32:16 -0400

    second

--- Routes/indexRouter.js
+++ Routes/indexRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const File = require('../Models/File');
 const path = require('path')
 router.get('/', (req, res) => {
-    res.json({ "name": "Deep" })
+    res.sendFile(path.resolve(__dirname, "./Client/build", "index.html"));
 })
 router.get('/api', (req, res) => {
     res.json({ "name": "Deep" })
